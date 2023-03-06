80ed05d9e8b78ba2eb0c8aa0f063f1f55794bbdf
AKhart1 <S200823@sggw.edu.pl>
2023-03-06 20:50:23 +0100

    Add project files.

---
+++ internetShop/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace internetShop.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string shortDesc { get; set; }
+        public string longDesc { get; set; }
+        public string img { get; set; }
+        public ushort price { get; set; }
+        public bool isFavourite { get; set; }
+        public int available { get; set; }
+        public int categoryID { get; set; }
+        public virtual Categories? Categories { get; set; }
+    }
+}
